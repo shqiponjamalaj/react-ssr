9b64d4856663f0eb6b4ff15251e3de2ee88ea6cb
root <shqiponjamalaj5@gmail.com>
2017-09-05 09:24:41 -0700

    useApiContext.ts added

---
+++ src/application/shared/lib/api/useApiContext.ts
@@ -0,0 +1,20 @@
+import { useAppLogger } from 'framework/public/universal';
+
+import { useConfig } from 'application/shared/config/hook';
+
+import { useRequest } from '../request';
+
+/**
+ * All useful things for any API
+ */
+export const useApiContext = () => {
+  const config = useConfig();
+  const logger = useAppLogger();
+  const request = useRequest();
+
+  return {
+    config,
+    logger,
+    request,
+  };
+};
