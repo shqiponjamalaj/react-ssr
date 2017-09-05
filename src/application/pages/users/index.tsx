8cb7bc06ec6f54b1bb5b65ec2b5830e8ccafe5c3
root <shqiponjamalaj5@gmail.com>
2017-09-05 14:25:38 -0700

    index.tsx added

---
+++ src/application/pages/users/index.tsx
@@ -0,0 +1,24 @@
+import { lazy } from 'react';
+
+import { CommonPage } from 'application/pages/shared';
+
+import { UserStatus } from 'application/entities/domain/user';
+
+export interface UsersPage extends CommonPage {
+  name: 'users';
+  params: {
+    page: number;
+    filterStatus?: UserStatus[];
+    activeUserId?: string;
+  };
+}
+
+export const usersPageDefaultParams: UsersPage['params'] = {
+  page: 1,
+  filterStatus: undefined,
+  activeUserId: undefined,
+};
+
+export { getTitle, getMetaData } from './metadata';
+
+export const UsersPage = lazy(() => import(/* webpackChunkName: "usersPage" */ './ui'));
