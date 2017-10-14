74b53bc4d077cd6857686a8b4ad7f251a745f95f
root <shqiponjamalaj5@gmail.com>
2017-10-14 16:56:22 -0700

    index.css.ts added

---
+++ src/application/shared/kit/toast/toasts/index.css.ts
@@ -0,0 +1,9 @@
+import { createStyles } from 'framework/public/styles';
+
+export const styles = createStyles({
+  item: {
+    position: 'absolute',
+    transition: 'transform ease 300ms',
+    width: '100%',
+  },
+});
