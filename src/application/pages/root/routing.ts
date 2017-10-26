c203ab16714964662e9e593044ed2dd1116fabb6
root <shqiponjamalaj5@gmail.com>
2017-10-26 13:20:23 -0700

    routing.ts updated

--- src/application/pages/root/routing.ts
+++
@@ -1,6 +0,0 @@
-import { RootPage } from '.';
-import { createRouteConfig } from '../_internals';
-
-export const rootPageRouteConfig = createRouteConfig<RootPage>({
-  mapURLParamsToPage: () => ({ name: 'root' }),
-});
