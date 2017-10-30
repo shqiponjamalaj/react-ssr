0dbdfa361eae3b02b41186740a0a583d07b90572
root <shqiponjamalaj5@gmail.com>
2017-10-30 10:20:28 -0700

    useCommonNavigate.ts added

---
+++ src/framework/infrastructure/router/hooks/useCommonNavigate.ts
@@ -0,0 +1,52 @@
+import { useCallback } from 'react';
+
+import { commonWithSelectors, noop, sequence } from 'framework/infrastructure/signal';
+
+import { useRouterReduxDispatch } from '../redux/hooks';
+import { selectAnyPage } from '../redux/selectors';
+import { patchPageSignal } from '../redux/signals/page';
+import { AnyPage } from '../types';
+
+export const useCommonNavigate = <AppPage extends AnyPage<string>>() => {
+  const dispatch = useRouterReduxDispatch();
+
+  /**
+   * Opens new pages and return a promise with that new page from a state
+   * Just a wrapper around redux part of the routing
+   * This wrapper allows to replace redux with something else,
+   * without an application deep refactoring
+   */
+  /**
+   * Patches new page and return a promise with updated page from a state
+   * Just a wrapper around redux part of the routing
+   * This wrapper allows to replace redux with something else,
+   * without an application deep refactoring
+   */
+  const navigate = useCallback(
+    <DestinationPage extends AppPage>(
+      destination: (activePage: AppPage) => DestinationPage,
+      useReplace = false,
+    ) =>
+      new Promise<DestinationPage>((resolve) => {
+        dispatch(
+          sequence(
+            patchPageSignal(destination as any, useReplace),
+            commonWithSelectors(
+              {
+                patchedPage: selectAnyPage,
+              },
+              ({ patchedPage }) => {
+                resolve(patchedPage as DestinationPage);
+                return noop();
+              },
+            ),
+          ),
+        );
+      }),
+    [dispatch],
+  );
+
+  return {
+    navigate,
+  };
+};
