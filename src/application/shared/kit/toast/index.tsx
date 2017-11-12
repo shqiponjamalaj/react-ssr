257286d1fbd7de3e59a34672946b7cd05ff26233
root <shqiponjamalaj5@gmail.com>
2017-11-11 18:18:50 -0800

    index.tsx updated

--- src/application/shared/kit/toast/index.tsx
+++
@@ -1,6 +0,0 @@
-export { ToastControllerContext } from './infrastructure/context';
-export { ToastController } from './infrastructure/controller';
-
-export { Toasts } from './toasts';
-
-export { useToast } from './infrastructure/hook';
