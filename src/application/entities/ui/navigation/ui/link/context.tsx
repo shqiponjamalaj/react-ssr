0eac9878c918cbde6ab333aaf92662454fde31c2
root <shqiponjamalaj5@gmail.com>
2018-01-17 18:08:39 -0800

    context.tsx updated

--- src/application/entities/ui/navigation/ui/link/context.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import { AnyAppContext } from 'framework/public/types';
-
-export const CompileAppURLContext = createContext<(appContext: AnyAppContext) => string>(() => {
-  return '/';
-});
