121e4c9bfbd4b1f9b38855e003c417ea67f0a67f
root <shqiponjamalaj5@gmail.com>
2018-02-07 13:32:00 -0800

    hook.ts added

---
+++ src/framework/infrastructure/session/hook.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+
+import { SessionContext } from './context';
+
+export const useSession = () => {
+  const session = useContext(SessionContext);
+
+  return session;
+};
