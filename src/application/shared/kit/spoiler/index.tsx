b8003eb39ea4070ff6a253e0ea1e1c9e2dff3f5e
root <shqiponjamalaj5@gmail.com>
2018-02-09 15:58:04 -0800

    index.tsx added

---
+++ src/application/shared/kit/spoiler/index.tsx
@@ -0,0 +1,24 @@
+import { useState, useCallback, memo } from 'react';
+
+import { DATA_T_ATTRIBUTE_NAME } from 'framework/public/universal';
+
+type Props = {
+  initiallyExpanded?: boolean;
+  onToggle?: (isExpanded: boolean) => void;
+  children: (isExpanded: boolean, toggle: () => void) => React.ReactNode;
+  [DATA_T_ATTRIBUTE_NAME]?: string;
+};
+export const Spoiler = memo<Props>((props) => {
+  const [isExpanded, setIsExpanded] = useState(!!props.initiallyExpanded);
+  const onToggle = props.onToggle;
+  const toggle = useCallback(() => {
+    if (onToggle) {
+      onToggle(!isExpanded);
+    }
+    setIsExpanded(!isExpanded);
+  }, [onToggle, isExpanded]);
+
+  const dtValue = props[DATA_T_ATTRIBUTE_NAME];
+
+  return <div data-t={dtValue}>{props.children(isExpanded, toggle)}</div>;
+});
