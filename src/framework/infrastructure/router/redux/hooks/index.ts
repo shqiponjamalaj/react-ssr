c16720e8a44804fead6308144aacb9af041688cf
root <shqiponjamalaj5@gmail.com>
2018-03-22 18:11:44 -0700

    index.ts updated

--- src/framework/infrastructure/router/redux/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { createDispatchHook, createSelectorHook } from 'react-redux';
-
-import { RouterReduxContext } from '../store/context';
-
-export const useRouterReduxDispatch = createDispatchHook(RouterReduxContext);
-export const useRouterReduxSelector = createSelectorHook(RouterReduxContext);
