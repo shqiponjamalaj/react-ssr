bd172518039368ec955fa4069d10bae93232b97e
root <shqiponjamalaj5@gmail.com>
2018-03-30 15:23:36 -0700

    server.ts added

---
+++ src/framework/infrastructure/logger/init/server.ts
@@ -0,0 +1,29 @@
+import pino from 'pino';
+const isProduction = process.env.NODE_ENV === 'production';
+
+function initLogger() {
+  const loggerInstance = pino({
+    base: null,
+    messageKey: 'message',
+    timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
+    formatters: {
+      level(label) {
+        return { pino_level: label };
+      },
+    },
+    transport: isProduction
+      ? {
+          target: 'pino/file',
+        }
+      : {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+          },
+        },
+  });
+
+  return loggerInstance;
+}
+
+export default initLogger();
