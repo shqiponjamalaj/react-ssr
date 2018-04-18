2e3324e284fa84b0e6052022ed7c7cf9bbaa9564
root <shqiponjamalaj5@gmail.com>
2018-04-18 11:41:34 -0700

    server.ts updated

--- src/framework/infrastructure/platform/cookie/server.ts
+++
@@ -1,5 +0,0 @@
-/* istanbul ignore next */
-import { createCookieAPI as createServerCookieAPI } from 'lib/cookies/server';
-
-/* istanbul ignore next */
-export const createCookieAPI = createServerCookieAPI;
