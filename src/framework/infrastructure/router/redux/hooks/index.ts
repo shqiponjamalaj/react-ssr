d65f0f14fa153fe6a9bc7c1f79ad394e43855093
root <shqiponjamalaj5@gmail.com>
2018-04-18 09:01:08 -0700

    index.ts added

---
+++ src/framework/infrastructure/router/redux/hooks/index.ts
@@ -0,0 +1,6 @@
+import { createDispatchHook, createSelectorHook } from 'react-redux';
+
+import { RouterReduxContext } from '../store/context';
+
+export const useRouterReduxDispatch = createDispatchHook(RouterReduxContext);
+export const useRouterReduxSelector = createSelectorHook(RouterReduxContext);
