7f45bb3954411a1f00dc153d3452bdd4cf7ca83b
root <shqiponjamalaj5@gmail.com>
2018-05-29 17:40:50 -0700

    startup.ts updated

--- src/framework/applications/client/store/startup.ts
+++
@@ -1,8 +0,0 @@
-import { historyReplace } from 'framework/infrastructure/router/redux/actions/router';
-import { createSignal } from 'framework/infrastructure/signal';
-
-/**
- * Signal, which will be dispatched before hydration
- * Caution, do not add any long tasks here!
- */
-export const startup = createSignal('clientStartup', () => historyReplace());
