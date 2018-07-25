4718c0e5ab6b8bc863ed580a35fc1b01d5954499
root <shqiponjamalaj5@gmail.com>
2018-07-25 15:35:48 -0700

    types.ts updated

--- src/framework/applications/client/types.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from '@tanstack/react-query';
-
-import { AnyPage, URLQueryParams } from 'framework/public/types';
-
-export type GetTitle<Page extends AnyPage<string>> = (params: {
-  queryClient: QueryClient;
-  page: Page;
-  URLQueryParams: URLQueryParams;
-}) => string;
