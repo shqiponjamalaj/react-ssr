c81dbe31a1eafdb4333a3faabed97d97a0b5fd0f
root <shqiponjamalaj5@gmail.com>
2018-07-28 17:42:56 -0700

    client.tsx updated

--- src/framework/public/client.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * Client-specific functions/constants are exported here
- *
- * You can use it on a client side only
- */
-
-export { startClientApplication } from '../applications/client';
-export { getClientApplicationConfig } from '../config/generator/client';
